54975087598f0abcd4da55e80d53b402647d8fd2
estshorter <1430311+estshorter@users.noreply.github.com>
2020-04-30 13:27:32 +0900

    Use close() instead of sending structt{}

--- timeout.go
+++ timeout.go
@@ -10,23 +10,24 @@ import (
 
 // Exec executes timeout
 func Exec(waitSecond int) {
+	fmt.Printf("\rWaiting for %v seconds, press any key to quit...", waitSecond)
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 	done := make(chan struct{})
-	timeoutTime := time.Now().Add(time.Second * time.Duration(waitSecond))
-	fmt.Printf("\rWaiting for %v seconds, press any key to quit...", waitSecond)
+	waitSecondDuration := time.Second * time.Duration(waitSecond)
+	timeoutTime := time.Now().Add(waitSecondDuration)
 
 	var once sync.Once
 	go func() {
-		time.Sleep(time.Duration(waitSecond) * time.Second)
-		once.Do(func() { done <- struct{}{} })
+		time.Sleep(waitSecondDuration)
+		once.Do(func() { close(done) })
 	}()
 
 	go func() {
 		if err := keyboard.Open(); err == nil {
 			keyboard.GetKey()
 		}
-		once.Do(func() { done <- struct{}{} })
+		once.Do(func() { close(done) })
 	}()
 	defer keyboard.Close()
 
